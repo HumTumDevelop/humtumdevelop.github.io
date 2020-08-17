d1d3a2eee69c122788a18ff4192ae6fa35847902
Majid <akhtarmajid273@gmail.com>
2020-08-17 23:27:44 +0530

    website version 0.2

---
+++ JS/humtum.js
@@ -0,0 +1,16 @@
+// document.getElementById("myDIV").style.display = "none";
+
+// function myFunction() {
+//   var x = document.getElementById("myDIV");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//     x.style.opacity = "1"
+//   } else {
+//     x.style.display = "none";
+//     x.style.opacity = "0"
+//   }
+// }
+
+function flip() {
+    $('.card').toggleClass('flipped');
+}
